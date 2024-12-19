1c449288c18a6e18905a4751eeb43c61a2c839e0
Udit Pratap Singh <153190852+uditsinghs@users.noreply.github.com>
2024-12-19 08:39:27 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,23 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
 import { FaCartArrowDown } from "react-icons/fa6";
 
+const Login = () => {
 
-const Login: React.FC = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Form Data:", formData);
+  };
+
   return (
     <div className="login-container">
       <div className="login-card">
         <div>
-          <FaCartArrowDown className="cart-icon"/>
+          <FaCartArrowDown className="cart-icon" />
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-field">
             <FaUser className="icon" />
-            <input type="text" placeholder="Username" required className="input"/>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              required
+              className="input"
+              value={formData.username}
+              onChange={handleChange}
+            />
           </div>
           <div className="input-field">
             <FaLock className="icon" />
-            <input type="password" placeholder="Password" required  className="input"/>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+              className="input"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </div>
           <button className="login-btn" type="submit">
             LOGIN
